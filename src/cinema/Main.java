fb49acc91e3864e82e92715ec174c01a06bf3980
Cristian De Abreu <cdeabreu907@gmail.com>
2025-02-09 23:30:02 -0400

    -test 2

--- src/cinema/Main.java
+++ src/cinema/Main.java
@@ -2,9 +2,19 @@ package cinema;
 
 public class Main {
     public static void main(String[] args) {
+        String text = "";
         int[][][] movies = new int[5][5][5];
         String[][] showtimes = new String[5][5];
         String[] name = new String[5];
+        int movie, times;
+
+        text = "Introduzca la cantidad de peliculas para el día de hoy";
+        movie = Validate.valSize(text);
+
+        text = "Introduzca la cantidad de horarios por pel";
+
+
+
         Process.iniInfo(movies);
         Process.iniShowtime(showtimes);
         Process.iniMovie(name);
