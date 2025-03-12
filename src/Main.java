e93d70a4309fa846d1ca6ded2b51b714d68c38bc
antorlok <andres.tortolero06gmail.com>
2025-03-12 18:46:12 -0400

    packages

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
-import java.io.IOException;
+import validateItem.Validate;
+import process.processMain;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        String text = "";
+        int[][][] movieInfo;
+        String[][] showtimes;
+        String[] movies;
+        int movie, times = 0;
+        String route = System.getProperty("user.dir")+"/src/cinema/Report.txt";
 
 
+
+        // Peticion de longitud de los arreglos
+
+        text = "\nIntroduzca la cantidad de peliculas para el día de hoy: ";
+        movie = Validate.valSize(text);
+
+        text = "\nIntroduzca la cantidad de horarios por pelicula: ";
+        times = Validate.valSize(text);
+
+
+        // Instanciacion de Arreglos
+        movies = new String[movie];
+        showtimes = new String[movie][times];
+        movieInfo = new int[movie][times][3];
+
+        //desarrollo
+        processMain.process(movieInfo,showtimes,movies);
+
+        movieInfo = null;
+        showtimes = null;
+        movies = null;
     }
 }
--- src/process/processMain.java
+++ src/process/processMain.java
@@ -1,6 +1,17 @@
 package process;
 
 public class processMain {
+    public static  void process(int triArray[][][], String biArray[][], String strArray[]){
+        //Inicio de Arrglos
+        process.iniSeats(triArray);
+        process.iniShowtime(biArray);
+        process.iniMovie(strArray);
 
+        //Process desarrollo
+        process.attachData(biArray, strArray);
+        process.showCase(strArray,biArray);
+        process.buyTicket(strArray,biArray,triArray);
+        process.showMenu(strArray,biArray,triArray);
+    }
 
 }
