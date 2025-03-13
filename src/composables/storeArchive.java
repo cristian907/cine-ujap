f5ff8769d5afd09d3c163dd5dcd127591944ab2f
antorlok <andres.tortolero06gmail.com>
2025-03-13 01:13:18 -0400

    packages

--- src/composables/storeArchive.java
+++ src/composables/storeArchive.java
@@ -5,7 +5,7 @@ import validateItem.Validate;
 public class storeArchive {
     public static void storeItemMovie(String movie[], String showTime[][], int nfo[][][], String root) {
         String text = "";
-        String route = root + "resultado.txt";
+        String route = root + "storeMovie.txt";
 
         if (movie != null && showTime != null) {
             for (int i = 0; i < showTime.length; i++) {
