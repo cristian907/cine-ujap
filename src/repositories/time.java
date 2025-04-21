2d0c01ebe1b9b5b747f86e69b39f002625203d05
Ramon Gomez <ramonomezflorez@gmail.com>
2025-04-21 15:43:11 -0400

    objetos "time" y "movie" 1.0 cargados completamente

--- src/repositories/time.java
+++ src/repositories/time.java
@@ -1,4 +1,64 @@
 package repositories;
 
 public class time {
+
+    private String iniHour;
+    private String finHour;
+    private String serialArchiveNumber;
+
+    public time(String iniHour, String finHour, String serialArchiveNumber) throws IllegalArgumentException{
+
+        this.utilValiniHour(iniHour);
+        this.utilValfinHour(finHour);
+        this.serialArchiveNumber = (serialArchiveNumber.isEmpty()) ? null: serialArchiveNumber;
+
+        if (this.iniHour == null || this.serialArchiveNumber == null || this.finHour == null){
+            throw new IllegalArgumentException("- Error-Instancia: Objeto incompleto. ");
+        }
+
+    }
+
+    public String getIniHour() {
+        return iniHour;
+    }
+
+    public void setIniHour(String hour) {
+        this.iniHour = hour;
+    }
+
+    public String getFinHour() {
+        return finHour;
+    }
+
+    public void setFinHour(String hour) {
+        this.finHour = hour;
+    }
+
+    public String getSerialArchive(){
+        return this.serialArchiveNumber;
+    }
+
+    public void setSerialArchive(String serial){
+        this.serialArchiveNumber = (serial.isEmpty()) ? null: serial;
+    }
+
+    private void utilValiniHour(String hour) throws IllegalArgumentException{
+
+        if (!hour.matches("\\d{1,2}:\\d{2}")){
+            throw new IllegalArgumentException(" [la hora debe estar escrita en formato hh:mm] ");
+
+        }else{
+            this.iniHour=hour;
+        }
+    }
+
+    private void utilValfinHour(String hour) throws IllegalArgumentException{
+
+        if (!hour.matches("\\d{1,2}:\\d{2}")){
+            throw new IllegalArgumentException(" [la hora debe estar escrita en formato hh:mm] ");
+
+        }else{
+            this.finHour=hour;
+        }
+    }
 }
